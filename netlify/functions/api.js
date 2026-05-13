62168b89a359355dcefcaf999c7a639750ac009a
Your Name <ankitsingh91040@gmail.com>
2026-05-14 01:08:57 +0530

    fix: move netlify.toml to root and configure serverless basePath

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http");
 const app = require("../../server/index.js");
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, { basePath: '/.netlify/functions' });
